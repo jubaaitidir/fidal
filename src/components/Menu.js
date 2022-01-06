faa60357c021a9cff2d73eff997dc9774c8b8c15
jubaaitidir <jubaaitidir@gmail.com>
2022-01-06 18:04:46 +0100

    panier v1 (echange composant)

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import ReactDOM from 'react-dom';
-import { Button, Navbar, Nav, NavDropdown, Container } from 'react-bootstrap';
+import { Button, Navbar, Nav, NavDropdown, Container, Offcanvas } from 'react-bootstrap';
 import logo from '../images/logo.png';
 //import chaussures from './chaussures.jpg'
 import Products from './Products';
@@ -11,19 +11,43 @@ import Panier from './Panier';
 
 
 
-function Menu({products}) {
+
+function get_list_prod(shoppingList) {
+    const list_shopping = shoppingList;
+    const list_prod = list_shopping.map((el) => {
+
+        <h2>{el.name_prod}</h2>
+
+        console.log(el);
+    })
+    console.log(list_prod);
+    console.log(list_shopping);
+    return (<Container>{list_prod}</Container>);
+
+
+}
+
+function Menu({ products }) {
+    // const { products } = Infos;
+    const [show, setShow] = useState(false);
+    const [shoppingList, setShoppingList] = useState([]);
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
+
     // alert(`selected ${eventKey}`)
     const handleSelect = (eventKey) => {
         switch (eventKey) {
-            case "1":  {
+            case "1": {
                 ReactDOM.render(
                     <React.StrictMode>
-                        <Home products={products}/>
+                        <Home products={products} />
 
                     </React.StrictMode>,
                     document.getElementById('main')
                 )
-                 break;}
+                break;
+            }
             case "2": {
                 ReactDOM.render(
                     <React.StrictMode>
@@ -37,8 +61,8 @@ function Menu({products}) {
             case "3": {
                 ReactDOM.render(
                     <React.StrictMode>
-                        
-                        <Products products={products} />
+
+                        <Products products={products} shoppingList={shoppingList} setShoppingList={setShoppingList} />
 
                     </React.StrictMode>,
                     document.getElementById('main')
@@ -58,7 +82,8 @@ function Menu({products}) {
             case "5": {
                 ReactDOM.render(
                     <React.StrictMode>
-                        <Panier/>
+                        <Panier show={show} setShow={setShow} handleClose={handleClose} shoppingList={shoppingList} setShoppingList={setShoppingList} />
+
 
                     </React.StrictMode>,
                     document.getElementById('main')
@@ -105,11 +130,29 @@ function Menu({products}) {
                     Equipe
                 </Nav.Link>
             </Nav.Item>
-            
-            <hr style={{color:'red'}}/>
+
+            <hr style={{ color: 'red' }} />
             <Nav.Item className='md-6' rounded>
-                <Nav.Link eventKey="5" title="panier" style={{ color: 'white' , backgroundColor: 'green', borderRadius:'5px'}} >
-                    panier
+                <Nav.Link eventKey="5" title="panier" style={{ color: 'white', borderRadius: '5px' }} >
+                    <Button variant="warning" onClick={handleShow}>
+                        Panier
+                    </Button>
+                    <Offcanvas show={show} onHide={handleClose}>
+                        <Offcanvas.Header closeButton>
+                            <Offcanvas.Title>Panier</Offcanvas.Title>
+                        </Offcanvas.Header>
+                        <Offcanvas.Body>
+                            <div className='container-fluid'>{(shoppingList.length == 0) ? "Votre panier est vide"
+                                : shoppingList.map((el) =>
+                                    <div className='row bg-primary mt-2'>
+                                        <div className='col-4 border-black'>{el.name_prod}</div>
+                                        <div  className='col-4'>{el.price} euros</div>
+                                        <div  className='col-4 badge bg-warning'>{(el.isSpecialOffer === true) && "Soldes"} </div>
+                                    </div>)}
+                            </div>
+
+                        </Offcanvas.Body>
+                    </Offcanvas>
                 </Nav.Link>
             </Nav.Item>
 
--- src/components/Products.js
+++ src/components/Products.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import { Container, Image, Row, Col, Button } from 'react-bootstrap';
 import { ReactDOM } from 'react';
 
@@ -14,25 +14,64 @@ function importAll(r) {
         images[item.replace('./', '')] = r(item);
         console.log(item);
     });
-    console.log(images);
+    //console.log(images);
 
     return images;
 }
 
+// function handleClick(prod) {
+//     alert(`je veux acheter le produit ${prod}`);
+//     setShoppingList({prod})
 
-function Products({ products }) {
+// }
+
+
+// function Example() {
+//     const [show, setShow] = useState(false);
+
+//     const handleClose = () => setShow(false);
+//     const handleShow = () => setShow(true);
+
+//     return (
+//         <>
+//             <Button variant="primary" onClick={handleShow}>
+//                 Launch
+//             </Button>
+
+//             <Offcanvas show={show} onHide={handleClose}>
+//                 <Offcanvas.Header closeButton>
+//                     <Offcanvas.Title>Offcanvas</Offcanvas.Title>
+//                 </Offcanvas.Header>
+//                 <Offcanvas.Body>
+//                     Some text as placeholder. In real life you can have the elements you
+//                     have chosen. Like, text, images, lists, etc.
+//                 </Offcanvas.Body>
+//             </Offcanvas>
+//         </>
+//     );
+// }
+
+// render(<Example />
+// );
+
+
+
+function Products({ products, shoppingList, setShoppingList}) {
+    //const [shoppingList, setShoppingList] = useState([]);
     //const { products } = Infos;
     let list_products = products;
     //console.log("****************" + window.location.origin)
     let images = importAll(require.context('../images/', false, /\.(png|jpe?g|svg)$/));
 
+ 
+
     //console.log(list_products.forEach(el => (console.log(el.img_prod_src))));
     let rows_products = list_products.map((product) => (
-        <div className='row justify-content-center' key={product.code_prod}>
+        <div className='row justify-content-center' >
             <div className='col-lg-6'>
                 <Container className='border border-primary rounded mt-2' fluid>
                     <Row className='mt-2'>
-                        <Image className='mt-2' src={images[product.img_prod_src].default} alt="" fluid rounded/>
+                        <Image className='mt-2' src={images[product.img_prod_src].default} alt="" fluid rounded />
                     </Row>
                     <Row className='mt-2'>
                         <Col >
@@ -41,7 +80,7 @@ function Products({ products }) {
                         </Col>
                         <Col>
                             <Container className='d-flex justify-content-center'>
-                                <Button className='d-flex justify-content-center' bg="light" variant="danger">commander</Button>
+                                <Button className='d-flex justify-content-center' bg="light" variant="danger" onClick={() => {shoppingList.push(product);setShoppingList(shoppingList); console.log(shoppingList)}}>commander</Button>
                             </Container>
                         </Col>
 
@@ -56,11 +95,12 @@ function Products({ products }) {
                         </Col>
                     </Row>
                 </Container>
+         
 
 
             </div>
         </div>
-    
+
     ));
 
     return (
